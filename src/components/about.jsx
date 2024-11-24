abcd1d6b4d92a83d1127aa288266667b54561254
ryanneozx <ryanneozx@gmail.com>
2024-11-24 19:16:54 +0800

    added webhooksto sections of page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,6 @@
 const About = (props) => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
+        <div id="about" name="about" className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img
                 src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
