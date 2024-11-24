aa6e8b9c5b7f0a476e4e29df951f05a2f2acad8b
ryanneozx <ryanneozx@gmail.com>
2024-11-24 18:06:29 +0800

    Added typewriter animation

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,5 +1,6 @@
 import githubLogo from './assets/github.svg'
 import linkedinLogo from './assets/linkedin.svg'
+import Themes from './navthemes'
 
 const Navbar = (props) => {
     return(
@@ -37,77 +38,15 @@ const Navbar = (props) => {
                 </div>
                 <div className="navbar-end">
                     {/* Socials Icon Links */}
-                    <a href="https://github.com/ryanneozx" className='p-1' target="_blank">
-                        <img src={githubLogo} className="h-8 w-8 color-primary fill-primary stroke-primary caret-primary accent-primary outline-primary fill-primary" alt="Github Social" />
+                    <a href="https://github.com/ryanneozx" className='p-1 svg' target="_blank">
+                        <img src={githubLogo} className="h-8 w-8 color-primary svg" alt="Github Social" />
                     </a>
                     <a href="https://www.linkedin.com/in/ryan-neo/" className='p-1' target="_blank">
-                        <img src={linkedinLogo} className="h-8 w-8" color='black' alt="Linkedin Social" />
+                        <img src={linkedinLogo} className="h-8 w-8" alt="Linkedin Social" />
                     </a>
                     {/* <button className="btn btn-primary mt-4" data-toggle-theme="coffee,night" data-act-class="ACTIVECLASS">Switch Theme</button> */}
                     {/* Theme Dropdown Menu (for testing) */}
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn m-1">
-                            Theme
-                            <svg
-                            width="12px"
-                            height="12px"
-                            className="inline-block h-2 w-2 fill-current opacity-60 "
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 2048 2048">
-                            <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-                            </svg>
-                        </div>
-                        <ul tabIndex={0} className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl">
-                            <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Default (Coffee)"
-                                value="coffee" />
-                            </li>
-                            <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Night"
-                                value="night" />
-                            </li>
-                            <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Retro"
-                                value="retro" />
-                            </li>
-                            <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Cyberpunk"
-                                value="cyberpunk" />
-                            </li>
-                            <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Valentine"
-                                value="valentine" />
-                            </li>
-                            <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Aqua"
-                                value="aqua" />
-                            </li>
-                        </ul>
-                        </div>
+                    <Themes></Themes>
                 </div>
             </div>
             {/* Page content here */}
