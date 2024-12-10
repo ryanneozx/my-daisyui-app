8998f8d0937ddb03ca879460d6222ab4b78e4858
ryanneozx <ryanneozx@gmail.com>
2024-12-10 17:05:55 +0800

    Removed edited links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -31,9 +31,9 @@ const Navbar = (props) => {
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about-me">About</a></li>
                     <li><a href="#experience">Experience</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a>Education</a></li>
                     <li><a>Certifications</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    
                     </ul>
                 </div>
                 <div className="navbar-end">
