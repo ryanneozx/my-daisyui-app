9181e241ee9feb6fa85db68a871e4f0b0ecc6679
ryanneozx <ryanneozx@gmail.com>
2024-12-10 18:13:38 +0800

    removed other themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   plugins: [require('daisyui')],
   daisyui: {
     // themes: ['light', 'dark', 'cupcake'], // List of themes you want to use
-    themes: true,  // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ['light', 'dark', 'dim', 'dracula'],  // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
