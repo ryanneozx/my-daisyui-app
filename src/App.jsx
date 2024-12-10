4564239a31226f2b8b06ee933cb64d0541f48974
ryanneozx <ryanneozx@gmail.com>
2024-12-10 18:13:55 +0800

    removed default daisyui stuff and unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar'
 import Hero1 from './components/hero1'
 import About from './components/about'
 import Experience from './components/experience'
 import Projects from './components/projects'
-import Experience2 from './components/experience2'
 
 function App() {
 
   return (
+    
     <Navbar>
       <Hero1></Hero1>
       <About></About>
       <Experience></Experience>
       <Projects></Projects>
-          <div className="p-6">
-
-            <h1 className="text-3xl font-bold text-center">Hello DaisyUI!</h1>
-            
-            <button className="btn btn-secondary mt-4">Click Me</button>
-            <button className="btn btn-accent">Button</button>
-          </div>
-            <footer className="footer footer-center bg-base-100 text-base-content p-4">
-              <aside>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved by Ryan Neo.</p>
-              </aside>
-            </footer>
+      <footer className="footer footer-center bg-base-100 text-base-content p-4">
+        <aside>
+          <p>Copyright © {new Date().getFullYear()} - All right reserved by Ryan Neo.</p>
+        </aside>
+      </footer>
     </Navbar>
     
   )
