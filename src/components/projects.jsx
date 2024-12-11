01c54310610a11ce4c4dfd2f2fc7ac42b43a26ab
ryanneozx <ryanneozx@gmail.com>
2024-12-11 19:40:57 +0800

    fixed carousel anchor

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -60,31 +60,31 @@ const Projects = (props) => {
             
             <div ref={(el) => (elementsRef.current[1] = el)} className="delayed-floating">
                 <div className="carousel w-4/5 rounded-box">
-                    <div id="slide1" className="carousel-item relative w-full ">
+                    <div id="slide1" className="carousel-item relative w-full anchor">
                         <img
                         src="src/assets/test.png"
                         className="w-full" />
                         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide3" className="btn btn-circle anchor">❮</a>
-                            <a href="#slide2" className="btn btn-circle anchor">❯</a>
+                            <a href="#slide3" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
                         </div>
                     </div>
-                    <div id="slide2" className="carousel-item relative w-full">
+                    <div id="slide2" className="carousel-item relative w-full anchor ">
                         <img
                         src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
                         className="w-full" />
                         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide1" className="btn btn-circle anchor">❮</a>
-                            <a href="#slide3" className="btn btn-circle anchor">❯</a>
+                            <a href="#slide1" className="btn btn-circle">❮</a>
+                            <a href="#slide3" className="btn btn-circle">❯</a>
                         </div>
                     </div>
-                    <div id="slide3" className="carousel-item relative w-full">
+                    <div id="slide3" className="carousel-item relative w-full anchor">
                         <img
                         src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
                         className="w-full" />
                         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide2" className="btn btn-circle anchor">❮</a>
-                            <a href="#slide1" className="btn btn-circle anchor">❯</a>
+                            <a href="#slide2" className="btn btn-circle">❮</a>
+                            <a href="#slide1" className="btn btn-circle">❯</a>
                         </div>
                     </div>
                 </div>
